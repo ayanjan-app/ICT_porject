a98ae874c329d88c74b10a51fb36a0a59fb69dff
rafi <rafiraf4121@gmail.com>
2025-12-18 20:48:49 +0500

    add the owl project

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
+git config --global user.name "rafi"
+git config --global user.email "rafiraf4121@gmail.com"
